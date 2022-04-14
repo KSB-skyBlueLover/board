f5cb8e7b80f8383216e2dd6f0ccf9c1cdaa8d898
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-04-14 19:10:57 +0900

    display board site

--- src/main/java/com/sangbeom/board/board/mapper/BoardMapper.java
+++ src/main/java/com/sangbeom/board/board/mapper/BoardMapper.java
@@ -2,8 +2,11 @@ package com.sangbeom.board.board.mapper;
 
 import com.sangbeom.board.board.dto.BoardDto;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
+@Mapper
 public interface BoardMapper {
     List<BoardDto> selectBoardList() throws Exception;
 }
